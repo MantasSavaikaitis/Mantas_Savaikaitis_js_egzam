ad6f4ed394c2c32890c9253e672f6890913d52ca
Mantas <sananotnyo@gmail.com>
2022-08-18 15:36:24 +0300

    task 8 and 7 done

--- Task 7/script.js
+++ Task 7/script.js
@@ -4,9 +4,19 @@ Turimas "audi" objektas.
 Parašykite funkciją "showObjectKeys", kuri kaip argumentą priims objektą 
 ir grąžins visus jo "key" masyve.
 -------------------------------------------------------------------------- */
+'use strict';
+console.log('script.js');
+
 const audi = {
   make: 'audi',
   model: 'A6',
   year: 2005,
   color: 'white',
 };
+
+function showObjectKeys(obj) {
+  // naudojame Object.keys() sukurti masyva su visais objekto "keys" ir ji graziname
+  return Object.keys(obj);
+}
+
+console.log('showObjectKeys ===', showObjectKeys(audi));
--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,22 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+'use strict';
+console.log('script.js');
+
+class Calculator {
+    constructor(a, b) {
+        this.sum = a + b;
+        this.subtraction = a - b;
+        this.multiplication = a * b;
+        this.division = a / b;
+    }
+}
+
+const calc1 = new Calculator(6, 2);
+
+console.log('pvz calc1 === new Calculator(6, 2) ', calc1);
+console.log('calc1.division; ===', calc1.division);
+console.log('calc1.multiplication ===', calc1.multiplication);
+console.log('calc1.subtraction ===', calc1.subtraction);
+console.log('calc1.sum ===', calc1.sum);
