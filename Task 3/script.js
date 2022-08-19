ac1215defec3bfc5c67894dd879daa0a1b9a1f69
Mantas <sananotnyo@gmail.com>
2022-08-19 17:03:48 +0300

    task 3 js and css formed

--- Task 3/script.js
+++ Task 3/script.js
@@ -10,5 +10,64 @@ Paspaudus mygtuką "Show users":
 Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi 
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
-
+'use strict';
+console.log('script.js');
 const ENDPOINT = 'https://api.github.com/users';
+
+async function init() {
+    buttonFunctionalityActivation();
+}
+init();
+
+
+function buttonFunctionalityActivation() {
+    const btnEl = document.getElementById('btn');
+    btnEl.addEventListener('click', async () => {
+        document.getElementById('message').remove();
+        const dataObjArr = await extractData(ENDPOINT);
+        insertDataToHtml(dataObjArr);
+    });
+}
+
+async function extractData(jsonLink) {
+    try {
+        const resp = await fetch(jsonLink);
+        // console.log('resp ===', resp);
+        if (resp.ok) {
+            const parsedData = await resp.json();
+            // console.log('parsedData ===', parsedData);
+            return parsedData;
+        }
+
+    }
+    catch (err) {
+        console.log('err ===', err);
+    };
+}
+
+function insertDataToHtml(userObjArr) {
+    const divEl = document.getElementById('output');
+    divEl.classList.add('output');
+    // alfabetic sorting
+    userObjArr.sort((a, b) => (a.login.toLowerCase < b.login.toLowerCase) ? -1 : 1);
+    console.log('objArr ===', userObjArr);
+    userObjArr.forEach(userObj => {
+        // console.log('brandObj ===', brandObj);
+        divEl.append(brandCardHtml(userObj));
+
+    });
+
+
+}
+
+function brandCardHtml(userObjArr) {
+    const cardDivEl = document.createElement('div');
+    cardDivEl.className = 'card';
+    const loginNameEL = document.createElement('h3');
+    const avatarEl = document.createElement('img');
+    loginNameEL.textContent = userObjArr.login;
+    avatarEl.src = userObjArr.avatar_url
+    cardDivEl.append(avatarEl, loginNameEL);
+    return cardDivEl;
+}
+
